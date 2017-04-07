f029a829943a36ad368165e4f7f5d10e688119be
Will Viles <will@willviles.com>
2017-04-07 12:16:49 +0100

    Makes shim ESLint friendly

--- vendor/scrollmagic-shim.js
+++ vendor/scrollmagic-shim.js
@@ -1,7 +1,9 @@
-(function() {
-  /* globals define, ScrollMagic, self */
+/* eslint-env node:true */
 
-  function scrollMagicUndefined(attr) {
+(function() {
+  /* globals define, self */
+
+  function scrollMagicUndefined() {
     var error = "ScrollMagic is not available. Either the bower package is missing or your app is in Fastboot Mode.";
 
     function throwError() {
