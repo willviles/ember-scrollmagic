7a4370f728d1189a8baae5e2e9065281fd4a5fa9
Will Viles <will@willviles.com>
2017-04-07 12:11:40 +0100

    Removes bower dependency and adds ScrollMagic npm

--- index.js
+++ index.js
@@ -1,38 +1,71 @@
-/* eslint-env node */
+/* eslint node: true */
 'use strict';
+
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+var VersionChecker = require('ember-cli-version-checker');
+var path = require('path');
 
 module.exports = {
   name: 'ember-scrollmagic',
 
-  included: function(app) {
+  included(app) {
 
-    this._super.included(app);
-
-    this.app = app;
     this.addonConfig = this.app.project.config(app.env)['ember-scrollmagic'] || {};
-    this.addonBuildConfig = this.app.options['ember-scrollmagic'] || {};
 
-    if (!process.env.EMBER_CLI_FASTBOOT) {
+    if (!isFastBoot()) {
 
-      // Import main ScrollMagic library
-      this.app.import(app.bowerDirectory + '/scrollmagic/scrollmagic/uncompressed/ScrollMagic.js');
+      const vendor = this.treePaths.vendor;
 
-      // Import GSAP animation plugin
-      this.app.import(app.bowerDirectory + '/scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js');
+      // Main modules
+      app.import(vendor + '/scrollmagic/ScrollMagic.js');
+      app.import(vendor + '/scrollmagic/plugins/animation.gsap.js');
 
-      // Import indicator plugin
+      // Add indicators
       if (this.addonConfig.indicators) {
-        console.log('ADDING INDICATORS');
-        this.app.import(app.bowerDirectory + '/scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js');
+        app.import(vendor + '/scrollmagic/plugins/debug.addIndicators.js');
       }
+
+      // Expose `import` via a shim
+      app.import('vendor/scrollmagic-shim.js', {
+        exports: {
+          scrollmagic: ['default']
+        }
+      });
 
     }
 
-    this.app.import('vendor/scrollmagic-shim.js', {
-      exports: {
-        scrollmagic: ['default']
-      }
-    });
-
+    return this._super.included.apply(this, arguments);
   },
+
+  treeForVendor(vendorTree) {
+    var trees = [];
+
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
+
+    trees.push(moduleToFunnel('scrollmagic'));
+
+    return mergeTrees(trees);
+  },
+
 };
+
+function moduleToFunnel(moduleName, destination) {
+  destination = destination || moduleName;
+  return new Funnel(resolveModulePath(moduleName), {
+    destDir: destination
+  });
+}
+
+function resolveModulePath(moduleName) {
+  return path.dirname(require.resolve(moduleName));
+}
+
+// Checks to see whether this build is targeting FastBoot. Note that we cannot
+// check this at boot time--the environment variable is only set once the build
+// has started, which happens after this file is evaluated.
+function isFastBoot() {
+  return process.env.EMBER_CLI_FASTBOOT === 'true';
+}
