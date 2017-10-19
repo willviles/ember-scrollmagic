b13f124bd9a3616e223ce18215b62e7ddd9d120f
Will Viles <will@willviles.com>
2017-10-19 15:27:26 +0100

    Lays out content for all example pages

--- tests/dummy/app/examples.js
+++ tests/dummy/app/examples.js
@@ -5,6 +5,12 @@ let definitions = [{
   title: 'Basic Animations',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Isto modo, ne si avia quidem eius nata non esset. Sed ne, dum huic obsequor, vobis molestus sim. Nam, ut sint illa vendibiliora, haec uberiora certe sunt. Quorum altera prosunt, nocent altera.'
 }, {
+  title: 'Custom Animations',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Isto modo, ne si avia quidem eius nata non esset. Sed ne, dum huic obsequor, vobis molestus sim. Nam, ut sint illa vendibiliora, haec uberiora certe sunt. Quorum altera prosunt, nocent altera.'
+}, {
+  title: 'Pinned Items',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eam tum adesse, cum dolor omnis absit; Fortasse id optimum, sed ubi illud: Plus semper voluptatis? Quid ei reliquisti, nisi te, quoquo modo loqueretur, intellegere, quid diceret?'
+}, {
   title: 'Parallax Sections',
   description: 'Age sane, inquam. Nam bonum ex quo appellatum sit, nescio, praepositum ex eo credo, quod praeponatur aliis. Eadem nunc mea adversum te oratio est. Est enim effectrix multarum et magnarum voluptatum. Duo Reges: constructio interrete.'
 }, {
@@ -13,9 +19,6 @@ let definitions = [{
 }, {
   title: 'Section Slides',
   description: 'Age sane, inquam. Nam bonum ex quo appellatum sit, nescio, praepositum ex eo credo, quod praeponatur aliis. Eadem nunc mea adversum te oratio est. Est enim effectrix multarum et magnarum voluptatum. Duo Reges: constructio interrete.'
-}, {
-  title: 'Pinned Navigation',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eam tum adesse, cum dolor omnis absit; Fortasse id optimum, sed ubi illud: Plus semper voluptatis? Quid ei reliquisti, nisi te, quoquo modo loqueretur, intellegere, quid diceret?'
 }, {
   title: 'Component Scrolling',
   description: 'Age sane, inquam. Nam bonum ex quo appellatum sit, nescio, praepositum ex eo credo, quod praeponatur aliis. Eadem nunc mea adversum te oratio est. Est enim effectrix multarum et magnarum voluptatum. Duo Reges: constructio interrete.'
