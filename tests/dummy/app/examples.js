cdaec36c551beb40557b22d83e1b59dfe74efd3d
Will Viles <will@willviles.com>
2017-10-19 13:01:44 +0100

    Dynamically renders example template into documentation.examples.example

--- tests/dummy/app/examples.js
+++ tests/dummy/app/examples.js
@@ -1,4 +1,7 @@
-export default [{
+import { assign } from '@ember/polyfills';
+import { dasherize } from '@ember/string';
+
+let definitions = [{
   title: 'Basic Animations',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Isto modo, ne si avia quidem eius nata non esset. Sed ne, dum huic obsequor, vobis molestus sim. Nam, ut sint illa vendibiliora, haec uberiora certe sunt. Quorum altera prosunt, nocent altera.'
 }, {
@@ -20,3 +23,11 @@ export default [{
   title: 'Horizontal Scrolling',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eam tum adesse, cum dolor omnis absit; Fortasse id optimum, sed ubi illud: Plus semper voluptatis? Quid ei reliquisti, nisi te, quoquo modo loqueretur, intellegere, quid diceret?'
 }];
+
+export const examples = definitions.map(example => {
+  return assign({}, example, {
+    slug: dasherize(example.title)
+  });
+})
+
+export default examples;
