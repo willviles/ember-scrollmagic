b12daa1d3de9c54f89da06eaac563f7b0d0e44bf
Will Viles <will@willviles.com>
2017-10-19 15:31:49 +0100

    Adds Ember Router Scroll to manage scroll position on route change

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,7 +1,10 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
+import RouterScroll from 'ember-router-scroll';
+
+const Router = EmberRouter.extend(
+  RouterScroll, {
   location: config.locationType,
   rootURL: config.rootURL
 });
