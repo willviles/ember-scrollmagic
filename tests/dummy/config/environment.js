3c3ec2956885af433ceafaa1e7dc36429bf98026
Will Viles <will@willviles.com>
2017-10-27 12:37:12 +0100

    Added ember-cli-github-pages addon

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -77,6 +77,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.locationType = 'hash';
+    ENV.rootURL = '/ember-scrollmagic/';
     // here you can enable a production-specific feature
   }
 
